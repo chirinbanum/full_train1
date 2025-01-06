6ed6b27e515430599822ca80d9410b92b0f053c2
chirinbanum <chirinbanu2004@gmail.com>
2025-01-06 15:16:49 +0530

    2

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import NavBar from './components/funcomp/NavBar'
+// import NavBar from './components/funcomp/NavBar'
 import About from './components/funcomp/About';
 import Contact from './components/funcomp/Contact';
 import Gallery from './components/funcomp/Gallery';
