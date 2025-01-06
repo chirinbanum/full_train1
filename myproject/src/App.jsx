4e2f213d40841bb837ef629851baaabfe0aac577
chirinbanum <chirinbanu2004@gmail.com>
2025-01-06 15:18:36 +0530

    2

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <NavBar />
+        {/* s */}
         <Routes>
           {/* Define routes */}
           <Route path="/" element={<Home />} />
